a00ee893b84aa4ab89997b21657c5d3005f783a0
Tarun Mahajan <2922986+tango4567@users.noreply.github.com>
2024-09-17 10:22:51 +0530

    Commit code

--- src/car/rentalsystem/DatabaseConnection.java
+++ src/car/rentalsystem/DatabaseConnection.java
@@ -1,4 +1,3 @@
-
 package car.rentalsystem;
 
 import java.sql.Connection;
@@ -13,7 +12,7 @@ public class DatabaseConnection {
 
 	// Resuablitly, Portablity
 
-	static Connection Get(String url, String port, String dbName, String dbUserName, String dbPswd) {
+	static Connection createConnection(String url, String port, String dbName, String dbUserName, String dbPswd) {
 		Connection con = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
@@ -48,7 +47,11 @@ public class DatabaseConnection {
 			System.out.println(e);
 		}
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login", "root", "");
+			String url = "jdbc:mysql://10.0.0.17/login?useSSL=false&allowPublicKeyRetrieval=true";
+			String username = "root";
+		    String password = "Password@321";
+//			con = DriverManager.getConnection("jdbc:mysql://10.0.0.17:3306/login", "root", "Password@321");
+			con = DriverManager.getConnection(url, username, password);
 			System.out.println("Connection Done....");
 			String sql = "select * from users";
 			preparedStatement = con.prepareStatement(sql);
@@ -82,7 +85,7 @@ public class DatabaseConnection {
 			System.out.println(e);
 		}
 		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login", "root", "");
+			con = createConnection(null, null, null, null, null);
 			System.out.println("Connection Done....");
 			String sql = "select * from users";
 			preparedStatement = con.prepareStatement(sql);
@@ -101,12 +104,6 @@ public class DatabaseConnection {
 				Float price =resultSet.getFloat("price");
 		
 				
-				
-				
-				
-				
-				
-
 				System.out.println("ID: " + id + " First Name: " + first_name + " Last Name: " + last_name + "cr_id :"+ cr_id+ "cr_Name:" + cr_name + "manufacturing_year :" + manufacturing_year + "brand :" + cr_brand 
 						+"units:"+units+ "price:"+ price);
 				
@@ -115,78 +112,5 @@ public class DatabaseConnection {
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
-	}
-	 
-	
-		
-}
-
-
-
-
-package car.rentalsystem;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-public class DatabaseConnection {
-	
-	//Resuablitly, Portablity 
-
-	static Connection Get(String url, String port, String dbName, String dbUserName, String dbPswd) {
-		Connection con = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-
-		try {
-
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver load");
-		} catch (ClassNotFoundException e) {
-			System.out.println(e);
-		}
-		try {
-			con = DriverManager.getConnection("jdbc:mysql://" + url + ":" + port + "/" + dbName, dbUserName, dbPswd);
-		} catch (SQLException e) {
-			System.out.println(e);
-		}
-
-		return con;
-	}
-
-	static void databaseConnection() {
-
-		Connection con = null;
-		PreparedStatement preparedStatement = null;
-		ResultSet resultSet = null;
-
-		try {
-
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver load");
-		} catch (ClassNotFoundException e) {
-			System.out.println(e);
-		}
-		try {
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/login", "root", "");
-			System.out.println("Connection Done....");
-			String sql = "select * from users";
-			preparedStatement = con.prepareStatement(sql);
-			resultSet = preparedStatement.executeQuery();
-
-			while (resultSet.next()) {
-				int id = resultSet.getInt("user_id");
-				String first_name = resultSet.getString("first_name");
-				String last_name = resultSet.getString("last_name");
-
-				System.out.println("ID: " + id + ", First Name: " + first_name + " Last Name: " + last_name);
-			}
-
-		} catch (SQLException e) {
-			System.out.println(e);
-		}
-	}
+	}		
 }
--- src/car/rentalsystem/Login.java
+++ src/car/rentalsystem/Login.java
@@ -3,14 +3,11 @@ package car.rentalsystem;
 import java.sql.Connection;
 
 public class Login {
-
 	public static void main(String[] args) {
-		 
-<<<<<<< HEAD
-		 DatabaseConnection.query(); // method calling without parameter
-=======
-		Connection connection = DatabaseConnection.getConnection(null, null, null, null, null);
->>>>>>> master
+		
+//		DatabaseConnection.databaseConnection();
+		
+		DatabaseConnection.query();
 	
 	}
   
